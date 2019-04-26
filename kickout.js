fd27161cd2ea4e8577b671664e21170022ef0a65
Ralf S. Engelschall <rse@engelschall.com>
2019-04-26 17:59:17 +0200

    bump year in all copyright messages

--- kickout.js
+++ kickout.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /*!
 **  Kickout -- Conveniently Release Git-Versioned NPM Package
-**  Copyright (c) 2017-2018 Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2017-2019 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **
 **  Permission is hereby granted, free of charge, to any person obtaining
 **  a copy of this software and associated documentation files (the
@@ -67,7 +67,7 @@ const UN         = require("update-notifier")
     if (argv.version) {
         process.stderr.write(my.name + " " + my.version + " <" + my.homepage + ">\n")
         process.stderr.write(my.description + "\n")
-        process.stderr.write("Copyright (c) 2017-2018 " + my.author.name + " <" + my.author.url + ">\n")
+        process.stderr.write("Copyright (c) 2017-2019 " + my.author.name + " <" + my.author.url + ">\n")
         process.stderr.write("Licensed under " + my.license + " <http://spdx.org/licenses/" + my.license + ".html>\n")
         process.exit(0)
     }
